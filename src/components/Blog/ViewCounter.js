076fd6ef5fe69d4d95ab89f82aa2e4b30d3cf1ed
GiangPC <truonggiang20001807@gmail.com>
2024-06-23 17:45:30 +0700

    ưip

--- src/components/Blog/ViewCounter.js
+++ src/components/Blog/ViewCounter.js
@@ -1,16 +1,16 @@
 "use client"
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 
-const ViewCounter = ({slug, noCount = false, showCount = true}) => {
+const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
 	const [views, setViews] = useState(0)
 
 	useEffect(() => {
 		if (!noCount) {
-			// incrementView()
+			setViews((pre) => pre + 1)
 		}
 	}, [slug, noCount])
 
-	useEffect(() => {}, [slug])
+	useEffect(() => { }, [slug])
 
 	if (showCount) {
 		return <div>{views} views</div>
