b0723764e629b54b5e2372c13d30325062b2e786
giangnvt <giangnvt@nami.trade>
2025-02-18 23:39:07 +0700

    wip

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,14 +6,14 @@ const siteMetadata = {
 	description: "Một trang Blog về lập trình và cuộc sống",
 	language: "vi-vn",
 	theme: "system", // system, dark or light
-	siteUrl: "https://lifecoding.dev/", // your website URL
+	siteUrl: "https://lifecoding.dev", // your website URL
 	siteLogo: "/logoWithBackground.png",
 	socialBanner: "/social_banner.png", // add social banner in the public folder
 	email: "truonggiang20001807@gmail.com",
 	github: "https://github.com/TruongGiang2000",
 	twitter: "https://twitter.com/code_bucks",
-	facebook: "https://www.facebook.com/rock.the.12914216/",
-	linkedin: "https://www.linkedin.com/in/827895219/",
+	facebook: "https://www.facebook.com/rock.the.12914216",
+	linkedin: "https://www.linkedin.com/in/827895219",
 	locale: "vi-VN",
 }
 
